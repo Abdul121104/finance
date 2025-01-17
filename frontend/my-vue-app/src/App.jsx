1f26b267b314d2c8e974a253ea1a58d91b8c8631
Abdul121104 <119029386+Abdul121104@users.noreply.github.com>
2025-01-17 21:20:06 +0530

    added view js route

--- frontend/my-vue-app/src/App.jsx
+++ frontend/my-vue-app/src/App.jsx
@@ -1,27 +1,24 @@
-import React from "react";  
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";  
-import Home from "./pages/Home";  
-import AddTransaction from "./pages/AddTransaction";
-import Advise from "./pages/Advise";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import AddTransaction from './pages/AddTransaction';
+import Advise from './pages/Advise';
+import ViewTransaction from './pages/ViewTransaction';
 
-const App = () => {  
-  return (  
+const App = () => {
+  return (
     <Router>
       <div>
-        <nav>
-          <Link to="/home"><button>Home</button></Link>
-          <Link to="/add-transaction"><button>Add Transaction</button></Link>
-          <Link to="/advise"><button>Financial Advisor</button></Link>
-        </nav>
 
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/add-transaction" element={<AddTransaction />} />
           <Route path="/advise" element={<Advise />} />
+          <Route path="/view-transaction" element={<ViewTransaction />} />
         </Routes>
       </div>
     </Router>
-  );  
-};  
+  );
+};
 
 export default App;
